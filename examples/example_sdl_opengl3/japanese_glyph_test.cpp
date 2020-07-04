2f204bf9d3e09dbc6ec93a36e3f390038f2b0140
vaiorabbit <vaiorabbit>
2020-07-04 23:34:20 +0900

    ShowStyleEditor test

--- examples/example_sdl_opengl3/japanese_glyph_test.cpp
+++ examples/example_sdl_opengl3/japanese_glyph_test.cpp
@@ -185,9 +185,10 @@ int main(int argc, char* argv[])
         ImGui::NewFrame();
 
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
+        if (show_demo_window) {
+            // ImGui::ShowDemoWindow(&show_demo_window);
+            ImGui::ShowStyleEditor();
+        }
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
 
         ImGui::SetNextWindowPos(ImVec2(5, 5), ImGuiCond_FirstUseEver);
